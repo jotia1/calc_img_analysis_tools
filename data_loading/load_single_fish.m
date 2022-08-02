2def5c46fe014f0a93a43af48e21ff128be0b86a
Joshua Arnold <j.arnold4@uq.edu.au>
2022-08-02 22:33:35 +1000

    Account for reclassified data

--- data_loading/load_single_fish.m
+++ data_loading/load_single_fish.m
@@ -1,4 +1,4 @@
-function [Suite2p_traces, ROI_centroids] = load_single_fish(pipeline_output_path, fish_number, load_s2p, load_rois)
+function [Suite2p_traces, ROI_centroids] = load_single_fish(pipeline_output_path, fish_number, load_s2p, load_rois, use_npys)
 %% LOAD_SINGLE_FISH - Load s2p and/or ANTs roi results for a fish into matlab
 %   Open folders in pipeline_output_path corresponding to fish_number with
 %   'suite2p_'/'ants_' and loading relevant suite2p/ants data. Assumes
@@ -15,6 +15,8 @@ function [Suite2p_traces, ROI_centroids] = load_single_fish(pipeline_output_path
 %       fish_number - zero-padded fish number of the fish to load (e.g. 05)
 %       load_s2p - true (default) or false if suite2p data should be loaded
 %       load_rois - true (default) or false if ROIs data should be loaded
+%       use_npys - true or false (default) if npys should be used instead
+%                   of Fall.mat to load iscell and F vars from suite2p.
 %   
 %
 %   Example usage:
@@ -28,6 +30,9 @@ end
 if ~exist('load_rois', 'var')
     load_rois = true;
 end
+if ~exist('use_npys', 'var')
+    use_npys = false;
+end
 
 %% Load Suite2p data
 fish_folder = dir(fullfile(pipeline_output_path, sprintf('suite2p_*fish%s*', fish_number)));
@@ -40,8 +45,18 @@ nplanes = numel(dir(fullfile(pipeline_output_path, sprintf('suite2p_*fish%s*', f
 if load_s2p
     for plane_idx = 0:nplanes-1 % planes index from 0 so subtract 1 from nplanes
         fprintf('fish%s, plane %d/%d\n', fish_number, plane_idx+1, nplanes);
-        filename = fullfile(pipeline_output_path, fish_folder.name, sprintf('plane%s', num2str(plane_idx)), 'Fall.mat'); %access the matfile of data
-        load(filename, 'F', 'iscell'); % just load fluorescene traces and centroid data from the matfile
+        
+        if use_npys
+            % used if data has been reclassified after original s2p
+            % reclassify only updates npy files, not Fall.mat
+            iscell_filename = fullfile(pipeline_output_path, fish_folder.name, sprintf('plane%s', num2str(plane_idx)), 'iscell.npy'); %access the matfile of data
+            F_filename = fullfile(pipeline_output_path, fish_folder.name, sprintf('plane%s', num2str(plane_idx)), 'F.npy'); %access the matfile of data
+            iscell = readNPY(iscell_filename);
+            F = readNPY(F_filename);
+        else
+            Fall_filename = fullfile(pipeline_output_path, fish_folder.name, sprintf('plane%s', num2str(plane_idx)), 'Fall.mat'); %access the matfile of data
+            load(Fall_filename, 'F', 'iscell'); % just load fluorescene traces and centroid data from the matfile
+        end
 
         Suite2p_traces = vertcat(Suite2p_traces, F(iscell(:,1) == 1,:)); % Add the traces to the list
     end
